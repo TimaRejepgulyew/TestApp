c0873d5adfd695830f70abbd573aa382e8c5ab37
TimaRejepgulyew <52398383+TimaRejepgulyew@users.noreply.github.com>
2022-07-13 16:00:04 +0500

    change file structure

---
+++ components/photoGalleryModule/layout/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <b-container fluid >
+            <b-row>
+                <b-col cols="2">
+                    <b-list-group v-if="!!users && users.length">
+                        <b-list-group-item v-for="user in users" :key="user.id">
+                            {{ user.name }}
+                            <NavList @changeAlbum="setAlbum" :userId="user.id"></NavList>
+                        </b-list-group-item>
+                    </b-list-group>
+                </b-col>
+                <b-col>
+                    <PhotoList v-if="albumId !== null" :albumId="albumId"></PhotoList>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+import Users from "@/fetchData/users"
+import NavList from "./navList.vue"
+import PhotoList from "../photoList"
+export default {
+    components: {
+        NavList,
+        PhotoList
+    },
+    data() {
+        return {
+            albumId: null
+        }
+    },
+    methods: {
+        setAlbum(id) {
+            this.albumId = id
+        }
+    },
+    computed: {
+        users() {
+            return Users
+        }
+    }
+}
+</script>
--- components/photoGalleryModule/layout/navList.vue
+++ components/photoGalleryModule/layout/navList.vue
@@ -1,6 +1,6 @@
 <template>
     <b-list-group v-if="!!albums && albums.length">
-        <b-list-group-item @click="$emit('changeAlbum', album.id)" v-for="album in albums" :key="album.id">
+        <b-list-group-item @click="$emit('changeAlbum', album.id)" v-for="album in albums" :key="album.id" :button="true">
             {{ album.title }}
         </b-list-group-item>
     </b-list-group>
