b4bdec06c0c3de57e7bce3846d400bf7c6c7dda2
TimaRejepgulyew <52398383+TimaRejepgulyew@users.noreply.github.com>
2022-07-13 15:58:44 +0500

    refatoring  forlder structure

--- pages/albums.vue
+++ pages/albums.vue
@@ -1,12 +1,23 @@
 <template>
- <div>
+  <div>
+    <div>
+      <!-- As a link -->
+      <b-navbar variant="faded" type="light">
+        <b-navbar-brand to="/">Home</b-navbar-brand>
+        <b-navbar-brand to="/albums">Albums</b-navbar-brand>
+      </b-navbar>
+    </div>
     <Content></Content>
- </div>
+  </div>
 </template>
 
 <script>
-import Content from "@/components/content"
+import Content from "@/components/photoGalleryModule/layout/index.vue"
+
 export default {
+  components: {
+    Content
+  },
   name: 'Albums'
 }
 </script>
